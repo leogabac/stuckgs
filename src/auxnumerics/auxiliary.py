7232f894f2d30957cbc7a0651dbaf94b2ca43490
leogabac <leogabac@gmail.com>
2024-08-21 22:17:58 -0600

    aux from reentrancy repo

--- src/auxnumerics/auxiliary.py
+++ src/auxnumerics/auxiliary.py
@@ -6,68 +6,84 @@
 
 import os
 import sys
-
-sys.path.insert(0, '../icenumerics/')
-import icenumerics as ice
-
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import math
+import itertools
+
+from tqdm import tqdm
+from math import isclose
+from numba import jit,prange,float64,int64,complex128
+
+# own
+sys.path.insert(0, '../icenumerics/')
+import icenumerics as ice
+import vertices as vrt
+
 
 ureg = ice.ureg
 idx = pd.IndexSlice
 
 
-def plotColloid(trj, frame):
-
-    """ 
-        Plots a particle system at a given frame for a given lammps trajectory.
-        Returns a (fig,ax).
+@jit(nopython=True)
+def get_idx_from_position(centers,pos,tol=0.1):
+    """
+        Get the index in the centers array from a position vector.
         ----------
-        Parameters:
-        * trj (pd Dataframe): lammps trajectory.
-        * frame
+        * centers: centers of the traps
+        * pos: np array with a 3D coordinate
     """
 
-    fig, ax = plt.subplots(figsize=(5, 5)); # Initialize
+    for i,center in enumerate(centers):
+        distance = np.linalg.norm(center - pos)
+        if np.isclose(0,distance,atol=tol):
+            return i
 
-    trj_particle = trj[trj.type==1]
-    trj_trap = trj[trj.type==2]
 
-    xparticle = np.array(trj_particle.loc[idx[frame,:],"x"])
-    yparticle = np.array(trj_particle.loc[idx[frame,:],"y"])
-    ax.plot(xparticle,yparticle,'o', color='y')
+def is_horizontal(direction):
+    """
+        Checks if a given direction is horizontal.
+        ----------
+        Parameters:
+        * direction
+    """
+    x = np.array([1,0,0])
+    dotP = np.dot(direction,x)
 
-    xtrap = np.array(trj_trap.loc[idx[frame,:],"x"])
-    ytrap = np.array(trj_trap.loc[idx[frame,:],"y"])
-    ax.plot(xtrap,ytrap,'o', color='g')
+    if isclose(abs(dotP),1,rel_tol=1e-3):
+        return True
+    else:
+        return False
 
-    ax.axis("square");
-    return fig,ax
 
-def save_colloid_fig(path,trj,frame):
-    
-    fig, ax = plt.subplots(figsize=(5, 5)); # Initialize
+@jit(nopython=True)
+def fix_position(position,a,size):
+    """
+        Fixes the position to fit in the box
+        0 < x < size*a, and 
+        0 < y < size*a
+        ----------
+        Parameters:
+        * position: Position vector in 3D
+        * a: lattice constant
+        * size: size of the system
+    """
+    L = size*a
 
-    trj_particle = trj[trj.type==1]
-    trj_trap = trj[trj.type==2]
+    # Apply BC to x
+    position[0] = position[0] % L
+    if position[0] < 0:
+        position[0] += L
 
-    xparticle = np.array(trj_particle.loc[idx[frame,:],"x"])
-    yparticle = np.array(trj_particle.loc[idx[frame,:],"y"])
-    ax.plot(xparticle,yparticle,'o', color='y')
+    # Apply BC to y
+    position[1] = position[1] % L
+    if position[1] < 0:
+        position[1] += L
 
-    xtrap = np.array(trj_trap.loc[idx[frame,:],"x"])
-    ytrap = np.array(trj_trap.loc[idx[frame,:],"y"])
-    ax.plot(xtrap,ytrap,'o', color='g')
+    return position
 
-    ax.axis("square");
-    
-    fig.savefig(path+str(frame)+".png",dpi=300)
-    plt.close(fig)
-    return None
-    
 
-def classifyVertices(vrt):
+def classify_vertices(vrt):
     """
         Classifies the vertices in I, II, III, IV, V, VI types.
         Returns a DataFrame
@@ -78,45 +94,16 @@ def classifyVertices(vrt):
 
     vrt["type"] = np.NaN
 
-    vrt.loc[vrt.eval("coordination==4 & charge == -4"),"type"] = "I"
-    vrt.loc[vrt.eval("coordination==4 & charge == -2"),"type"] = "II"
-    vrt.loc[vrt.eval("coordination==4 & charge == 0 & (dx**2+dy**2)==0"),"type"] = "III"
-    vrt.loc[vrt.eval("coordination==4 & charge == 0 & (dx**2+dy**2)>0"),"type"] = "IV" # Dipolo
-    vrt.loc[vrt.eval("coordination==4 & charge == 2"),"type"] = "V"
-    vrt.loc[vrt.eval("coordination==4 & charge == 4"),"type"] = "VI"
-    return vrt
-
-def classify_vrt_expanded(vrt):
-    """
-        Classifies the vertices in I, IIa, IIb, IIIa, IIIb, IVa, IVb, Va, Vb, VI types.
-        Returns a DataFrame
-        ----------
-        Parameters:
-        * vrt (pd Dataframe): Vertices df
-    """
-
-    vrt["type"] = ['tmp']*len(vrt)
-
-    vrt.loc[vrt.eval("coordination==4 & charge == -4"),"type"] = "I"
-
-    vrt.loc[vrt.eval("coordination==4 & charge == -2 & dx**2>0 & dy**2==0"),"type"] = "IIa"
-    vrt.loc[vrt.eval("coordination==4 & charge == -2 & dx**2==0 & dy**2>0"),"type"] = "IIb"
-
-    vrt.loc[vrt.eval("coordination==4 & charge == 0 & (dx**2+dy**2)==0"),"type"] = "III"
-
-    vrt.loc[vrt.eval("coordination==4 & charge == 0 & dx*dy>0"),"type"] = "IVa"
-
-    vrt.loc[vrt.eval("coordination==4 & charge == 0 & dx*dy<0"),"type"] = "IVb"
-
-    vrt.loc[vrt.eval("coordination==4 & charge == 2 & dx**2==0 & dy**2>0"),"type"] = "Va"
-    vrt.loc[vrt.eval("coordination==4 & charge == 2 & dx**2>0 & dy**2==0"),"type"] = "Vb"
-
-    vrt.loc[vrt.eval("coordination==4 & charge == 4"),"type"] = "VI"
+    vrt.loc[vrt.eval("coordination==4 & charge == -4"),"type"] = 1
+    vrt.loc[vrt.eval("coordination==4 & charge == -2"),"type"] = 2
+    vrt.loc[vrt.eval("coordination==4 & charge == 0 & (dx**2+dy**2)==0"),"type"] = 3
+    vrt.loc[vrt.eval("coordination==4 & charge == 0 & (dx**2+dy**2)>0"),"type"] = 4 # Dipolo
+    vrt.loc[vrt.eval("coordination==4 & charge == 2"),"type"] = 5
+    vrt.loc[vrt.eval("coordination==4 & charge == 4"),"type"] = 6
     return vrt
 
 
-def getVerticesDict(path):
-
+def vrt_dict(path):
     """
         Walks path and imports all DFs into a Dictionary, classifies the vertices and drops boundaries.
         Returns a dictionary with all the DataFrames.
@@ -130,18 +117,19 @@ def getVerticesDict(path):
     numberExperiments = len(files)
     for i in range(1,numberExperiments+1):
         filePath = os.path.join(path,f"vertices{i}.csv")
-        
+
         # Check if the file exists, if not, skips.
-        if not os.path.isfile(filePath):continue
-        
+        if not os.path.isfile(filePath):
+            continue
+
         vrt = pd.read_csv(filePath, index_col=[0,1])
-        vrt = classifyVertices(vrt)
+        vrt = classify_vertices(vrt)
         vrt = vrt.dropna()
         verticesExp[f"{i}"] = vrt
     return verticesExp
 
-def getVerticesCount(verticesDict):
-    
+
+def vrt_counts(verticesDict):
     """
         Loops the verticesDict with all experiments and gets the counts for vertex type
         Returns a dictionary with the counts DF for all experiments
@@ -154,11 +142,10 @@ def getVerticesCount(verticesDict):
     for key,experiment in verticesDict.items():
         currentCount = ice.count_vertices(experiment)
         countsDict[key] = currentCount
-    
     return countsDict
 
-def getVerticesAverage(counts,framerate):
-    
+
+def vrt_averages(counts,framerate):
     """
         Averages over all realizations.
         ----------
@@ -169,94 +156,34 @@ def getVerticesAverage(counts,framerate):
     # Get a list containing all the different frames
     allFrames = counts["1"].index.get_level_values('frame').unique().to_list()
     time = np.array(allFrames)/framerate
-    
     numberFrames = len(allFrames)
     numberRealizations = len(counts)
-    
 
-    fractions = pd.DataFrame(columns=["time","I","II","III","IV","V","VI"], data = np.zeros((numberFrames,7)))
+    fractions = pd.DataFrame(columns=["time","1.0","2.0","3.0","4.0","5.0","6.0"], data=np.zeros((numberFrames,7)))
 
     for key,experiment in counts.items():
         for vertexType,vrt in experiment.groupby("type"):
             vertexFraction = np.array(vrt.fraction)
-            fractions[vertexType] += vertexFraction
+            fractions[str(vertexType)] += vertexFraction
 
     fractions = fractions / numberRealizations
-    # fractions["theta"] = time * np.pi/2/60 * (180/np.pi)
     fractions["time"] = time
     return fractions
 
-def getPaintedFrame(trj,ctrj,frame,framerate):
 
-    """
-        Visualize the charges of a particular frame.
-        ----------
-        Parameters:
-        * trj (pd Dataframe): lammps trj
-        * ctrj (pd Dataframe): lammps ctrj
-        * frame
-        * framerate
-    """
-    v = ice.vertices()
-    v = v.trj_to_vertices(ctrj.loc[frame])
-    currentTime = frame / framerate
-    f,ax = plotColloid(trj,frame)
-    ax.set_title('t = {} s'.format(currentTime))
-    v.display(ax)
+def do_vertices(params,data_path):
 
-def saveAllPaintedFrames(trj,ctrj,frames,framerate,path):
-    
-    """
-        Save all painted frames of a simulation.
-        ----------
-        Parameters:
-        * trj (pd Dataframe): lammps trj
-        * ctrj (pd Dataframe): lammps ctrj
-        * frames: list of frames to export
-        * framerate
-    """
-    for frame in frames:
-        figPath = path + f"{frame}.png";
-        try:
-            getPaintedFrame(trj,ctrj,frame,framerate);
-            print(frame)
-            plt.savefig(figPath, dpi=300);
-            plt.close()
-        except:
-            print("skip")
-            continue
-    return None
-    
-def get_colloids_from_ctrj(ctrj,particle,trap,particle_radius,a,N):
+    vrt_path = os.path.join(data_path,"vertices/")
+    vertices = vrt_dict(vrt_path)
+    counts = vrt_counts(vertices)
+    vrt_ts = vrt_averages(counts,params["framespersec"].magnitude)
+    types = vrt_ts.columns.to_list()[1:]
+    t = vrt_ts["time"].to_numpy()
+    vrt_cuentas = vrt_ts[types].to_numpy()
+    return t, vrt_cuentas
 
-    """
-        Reconstruct the colloidal ice object from simulation parameters.
-        ----------
-        Parameters:
-        * ctrj (pd Dataframe): lammps ctrj without "t" and "type" columns
-        * particle: particle simulation object
-        * trap: trap simulation object
-        * particle_radius
-        * a: lattice constant
-        * N: system size
-    """
-    centers = [ row[:3].to_list() * ureg.um for _,row in ctrj.iterrows()]
-    directions = [ row[3:6].to_list() * ureg.um for _,row in ctrj.iterrows()]
-    arrangement = {
-        "centers" : centers,
-        "directions" : directions
-    }
 
-    col = ice.colloidal_ice(arrangement, particle, trap,
-            height_spread = 0, 
-            susceptibility_spread = 0.1,
-            periodic = True)
-    col.region = np.array([[0,0,-3*(particle_radius/a/N).magnitude],[1,1,3*(particle_radius/a/N).magnitude]])*N*a
-    
-    return col
-
-def get_colloids_from_ctrj2(ctrj,params):
-
+def trj2col(params,ctrj):
     """
         Reconstruct the colloidal ice object from simulation parameters.
         Notice that this version uses a params dict.
@@ -271,22 +198,22 @@ def get_colloids_from_ctrj2(ctrj,params):
     a = params["lattice_constant"]
     N = params["size"]
 
-    centers = [ row[:3].to_list() * ureg.um for _,row in ctrj.iterrows()]
-    directions = [ row[3:6].to_list() * ureg.um for _,row in ctrj.iterrows()]
+    centers = [row[:3].to_list() * ureg.um for _,row in ctrj.iterrows()]
+    directions = [row[3:6].to_list() * ureg.um for _,row in ctrj.iterrows()]
     arrangement = {
-        "centers" : centers,
-        "directions" : directions
+        "centers":centers,
+        "directions":directions
     }
 
     col = ice.colloidal_ice(arrangement, particle, trap,
-            height_spread = 0, 
-            susceptibility_spread = 0.1,
-            periodic = True)
+                            height_spread=0,
+                            susceptibility_spread=0.1,
+                            periodic=True)
     col.region = np.array([[0,0,-3*(particle_radius/a/N).magnitude],[1,1,3*(particle_radius/a/N).magnitude]])*N*a
-    
     return col
 
-def count_vertices_single(vrt, column = "type"):
+
+def vrtcount_sframe(vrt, column="type"):
     """
         Counts the vertices of a single frame df.
         ----------
@@ -297,12 +224,11 @@ def count_vertices_single(vrt, column = "type"):
     vrt_count = vrt.groupby(column).count().iloc[:,0]
     types = vrt_count.index.get_level_values(column).unique()
     counts = pd.DataFrame({"counts": vrt_count.values}, index=types)
-    counts["fraction"] = counts["counts"] / counts["counts"].sum() 
+    counts["fraction"] = counts["counts"] / counts["counts"].sum()
     return counts
 
 
-def get_vertices_last_frame(path,last_frame=2399):
-
+def vrt_lastframe(path,last_frame=2399):
     """
         Computes the vertices of only the last frame.
         ----------
@@ -322,13 +248,12 @@ def get_vertices_last_frame(path,last_frame=2399):
         v = ice.vertices()
         v = v.trj_to_vertices(ctrj)
     except:
-        get_vertices_last_frame(path,last_frame=last_frame-1)
+        vrt_lastframe(path,last_frame=last_frame-1)
   
-
     return v.vertices
 
-def get_vertices_at_frame(ctrj,frame):
 
+def vrt_at_frame(ctrj,frame):
     """
         Computes the vertices of a specific frame.
         ----------
@@ -337,18 +262,15 @@ def get_vertices_at_frame(ctrj,frame):
         * last: last frame of the simulation
     """
 
-    
     ctrj = ctrj.loc[idx[frame,:]].drop(["t", "type"],axis=1)
 
     v = ice.vertices()
     v = v.trj_to_vertices(ctrj)
-  
 
     return v.vertices
 
 
-def get_min_from_domain(f,domain):
-    
+def min_from_domain(f,domain):
     """
         Returns the value in domain that minimizes f.
         ----------
@@ -356,93 +278,13 @@ def get_min_from_domain(f,domain):
         * f: function
         * domain: iterable
     """
-    
+
     feval = [f(x) for x in domain]
     idx = np.argmin(feval)
     return domain[idx]
 
-def get_pbc_distance(params,xi,xj):
-    """
-        Returns the real distance between xi and xj in 3D space with PBC.
-        ASSUMES POSITION UNITS ARE IN MICROMETERS
-        ----------
-        Parameters:
-        * params: simulation parameters
-        * xi: particle position
-        * xj: particle position
-    """
-    
-    L = params['size']*params['lattice_constant'].magnitude
-    
-    # For reference, this line returns the true differences
-    # [ xi-xj , yi-yj , zi-zj ]
-    xij_pbc = np.array(list(map( lambda xu: get_min_from_domain(np.abs,[xu,xu+L,xu-L]), xi-xj )))
-    distance = np.sqrt(sum(xij_pbc**2))
-    
-    #return distance * params['lattice_constant'].units, xij_pbc/distance
-    return distance, xij_pbc/distance
 
-def dipole_pair_energy(params,xi,xj,Bhat = np.array([1,0,0])):
-    
-    """
-        Computes the magnetic dipole interaction energy between a pair of particles.
-        Returns a value in pN*nm
-       ----------
-        Parameters:
-        * params: simulation parameters
-        * xi: particle position
-        * xj: particle position
-        * Bhat: Magnetic field direction
-    """    
-    
-    # Get the distance and magnetic moment
-    distance, rhat = get_pbc_distance(params,xi,xj)
-    
-    #dimensional = (- params['mu0']*params['m']**2/4/np.pi/distance**3).to( ureg.pN * ureg.nm)
-    dimensional = (params['freedom']/distance**3)
-    adimensional = 3*Bhat.dot(rhat)**2 - 1
-    
-    
-    return (dimensional * adimensional)
-
-def calculate_energy(params,sel_particles):
-    
-    """
-        Computes the magnetic dipole total energy in the system
-        Returns a value in pN*nm
-       ----------
-        Parameters:
-        * params: simulation parameters
-        * sel_particles: some dataframe with id and x y z coordinates at given frame
-        * frame
-    """   
-    #particles = trj[trj.type==1]
-    #sel_particles = particles.loc[idx[frame,['x','y','z']]]
-    n = len(sel_particles)
-    H = sum(
-        dipole_pair_energy(params,
-                    sel_particles.loc[idx[i]].to_numpy(), 
-                    sel_particles.loc[idx[j]].to_numpy())
-        for i in range(1,n+1) for j in range(i+1,n+1)
-        )
-    
-    return H
-
-def get_coordinates_at_frame(trj,frame):
-    """
-        Given a trj file. Retrieves the the positions of the particles at a given frame.
-        ----------
-        Parameters:
-        * trj
-        * frame
-    """
-    
-    particles = trj[trj.type==1]
-    sel_particles = particles.loc[idx[frame,['x','y','z']]]
-    
-    return sel_particles
-
-def get_positions_from_ctrj(ctrj):
+def positions_from_trj(ctrj):
     """
         Given a ctrj file. Retrieves the positions of the particles.
         This is used to compute energy.
@@ -450,15 +292,14 @@ def get_positions_from_ctrj(ctrj):
         Parameters:
         * ctrj
     """
-    
-    x = (ctrj['x'] + ctrj['cx']).to_numpy() 
-    y = (ctrj['y'] + ctrj['cy']).to_numpy() 
+    x = (ctrj['x'] + ctrj['cx']).to_numpy()
+    y = (ctrj['y'] + ctrj['cy']).to_numpy()
     z = (ctrj['z'] + ctrj['cz']).to_numpy()
-    
-    stuff = pd.DataFrame(data =np.column_stack((x,y,z)), columns=['x','y','z'], index=list(range(1,len(ctrj)+1)))
+
+    stuff = pd.DataFrame(data=np.column_stack((x,y,z)), columns=['x','y','z'], index=list(range(1,len(ctrj)+1)))
     stuff.rename_axis('id', inplace=True)
-    
     return stuff
+
 
 def dropvis(ctrj):
     """
@@ -472,8 +313,7 @@ def dropvis(ctrj):
 
 def load_ctrj_and_vertices(params,data_path,size,realization = 1):
     """
-        Previously get_ctrj_and_vertices_from_file
-        Loads ctrj and vertices object.
+        Loads trj and vertices object.
         ----------
         Parameters:
         * params
@@ -482,7 +322,7 @@ def load_ctrj_and_vertices(params,data_path,size,realization = 1):
         * realization
     """
     params['size'] = size
-    ctrj = pd.read_csv(os.path.join(data_path,str(size),'ctrj','ctrj1.csv'),index_col=[0,1])
+    ctrj = pd.read_csv(os.path.join(data_path,str(size),'trj','trj1.csv'),index_col=[0,1])
     vrt = pd.read_csv(os.path.join(data_path,str(size),'vertices','vertices1.csv'),index_col=[0,1])
     last_frame = vrt.index.get_level_values('frame').unique()[-1]
     
@@ -531,7 +371,7 @@ def autocorrelation(ts):
 
 def correlate_bframes(params,ts,sframes, stime= 0, etime = 60):
     """
-        Computes the autocorrelations between some times (start,end)
+        Computes the autocorrelations between some times (start,enD)
         Returns an array in which the rows are the different particles.
         ----------
         Parameters:
@@ -554,3 +394,5 @@ def correlate_bframes(params,ts,sframes, stime= 0, etime = 60):
     
 def bint(x):
     return np.array([int(i) for i in x])
+
+
